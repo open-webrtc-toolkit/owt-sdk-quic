2b3a773b7b1d0185cefc32f63c772a9f9afc028c
Jianjun Zhu <jianjun.zhu@intel.com>
2020-08-10 18:59:46 +0800

    Build complete library to avoid extra dependencies.
    It also resolves OpenSSL symbol conflict because BoringSSL symbols are
    included in libowt_quic_transport.so.

---
+++ quic_transport/impl/quic_transport_factory_impl_unittest.cc
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2020 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "owt/quic/quic_transport_factory.h"
+#include "owt/quic_transport/impl/quic_transport_factory_impl.h"
+#include "base/logging.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace owt {
+namespace quic {
+namespace test {
+
+// Disabled because the certificate path is hard coded.
+TEST(DISABLED_QuicTransportFactoryImplTest, CreateQuicTransportServer) {
+  auto* factory = QuicTransportFactory::Create();
+  EXPECT_TRUE(factory != nullptr);
+  auto* server = factory->CreateQuicTransportServer(20001, "", "", "");
+  EXPECT_TRUE(server != nullptr);
+}
+
+}  // namespace test
+}  // namespace quic
+}  // namespace owt
