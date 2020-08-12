9b85486538a28e71b9b4b607c535a511a4104250
Jianjun Zhu <jianjun.zhu@intel.com>
2020-08-12 13:20:30 +0800

    Fix compile issues for dll on Windows.

---
+++ quic_transport/impl/dllmain.cc
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2020 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
+  return true;
+}
