c0c642c216550bd59bf6dc702c3be9968d7d58de
Jianjun Zhu <jianjun.zhu@intel.com>
2020-12-22 13:30:33 +0800

    Enforce unit tests during CI build.

--- test/scripts/build.py
+++ test/scripts/build.py
@@ -21,7 +21,9 @@ import zipfile
 SRC_PATH = Path(__file__).resolve().parents[3]
 PATCH_PATH = SRC_PATH/'owt'/'quic_transport'/'patches'
 PACKAGE_PATH = SRC_PATH.parent/'packages'
-SDK_TARGET_NAME = 'owt_quic_transport'
+SDK_TARGETS = ['owt_quic_transport']
+TEST_TARGETS = ['owt_quic_transport_tests']
+TEST_TARGETS_WIN = ['owt_quic_transport_dll_tests']
 PATCH_LIST = [
     ('0001-Add-owt_quic_transport-to-BUILD.gn.patch', SRC_PATH)
 ]
@@ -29,7 +31,7 @@ PATCH_LIST = [
 
 def sync():
     gclient_bin = 'gclient.bat' if sys.platform == 'win32' else 'gclient'
-    if subprocess.call([gclient_bin, 'sync'], cwd=SRC_PATH, shell=False):
+    if subprocess.call([gclient_bin, 'sync', '--reset'], cwd=SRC_PATH, shell=False):
         return False
     return True
 
@@ -56,12 +58,22 @@ def build():
     gn_args = {'debug': 'is_debug=true is_component_build=false symbol_level=1',
                'release': 'is_debug=false is_component_build=false'}
     gn_bin = 'gn.bat' if sys.platform == 'win32' else 'gn'
+    test_targets_on_this_platform = TEST_TARGETS
+    if sys.platform == 'win32':
+        test_targets_on_this_platform.extend(TEST_TARGETS_WIN)
+    targets = SDK_TARGETS+test_targets_on_this_platform
     for scheme, args in gn_args.items():
         output_path = SRC_PATH/'out'/scheme
         subprocess.call([gn_bin, 'gen', str(output_path), '--args=%s' % args])
-        if subprocess.call(['ninja', '-C', str(output_path), SDK_TARGET_NAME],
+        if subprocess.call(['ninja', '-C', str(output_path)]+targets,
                            cwd=SRC_PATH, shell=False):
             return False
+        for test_target in test_targets_on_this_platform:
+            test_path = output_path / \
+                (test_target + '.exe') if sys.platform == 'win32' else output_path/test_target
+            run_test = subprocess.run(test_path)
+            if run_test.returncode != 0:
+                return False
     return True
 
 
@@ -79,10 +91,11 @@ def pack():
 
     def pack_binaries(package_root):
         file_names = []
-        if sys.platform == 'win32':
-            file_names = [SDK_TARGET_NAME+'.dll', SDK_TARGET_NAME+'.dll.lib']
-        elif sys.platform == 'linux':
-            file_names = ['lib'+SDK_TARGET_NAME+'.so']
+        for sdk_target in SDK_TARGETS:
+            if sys.platform == 'win32':
+                file_names.extend([sdk_target+'.dll', sdk_target+'.dll.lib'])
+            elif sys.platform == 'linux':
+                file_names.extend(['lib'+sdk_target+'.so'])
         for scheme in ['debug', 'release']:
             Path.mkdir(package_root/'bin'/scheme, parents=True)
             for file_name in file_names:
